88d92d0ad0c90f9f22807f4a2c89208dd397bc90
Hibana <pagliaccizeus@gmail.com>
2021-07-01 17:34:22 +0300

    add translate to polish and ukrainian and fixed bugs

--- src/routes/home/Components/RadioToggleLanguage.js
+++ src/routes/home/Components/RadioToggleLanguage.js
@@ -1,5 +1,65 @@
-import React from "react";
+import React, { Component } from "react";
+import ArrowBackIosOutlinedIcon from "@material-ui/icons/ArrowBackIosOutlined";
 
-export default function RadioToggleLanguage() {
-  return <div className=""></div>;
+// import { FormControl, InputLabel, Select, MenuItem } from "@material-ui/core";
+
+class RadioToggleLanguage extends Component {
+  state = {
+    isActive: false,
+  };
+
+  handleClickSelectLanguage = (event) => {
+    this.props.onChangeLanguageHandler(event.target.dataset.source);
+  };
+
+  toggleDropdownMenu = () => {
+    let { isActive } = this.state;
+
+    this.setState({ isActive: !isActive });
+  };
+
+  render() {
+    let { isActive } = this.state;
+    let { currentLanguage } = this.props;
+    return (
+      <div className="checkboxSelectLanguage">
+        <div className="dropDownMenu">
+          <div className="dropdownTitle">
+            <h2
+              // className="selectionCurrentLanguage"
+              className={
+                isActive
+                  ? "selectionCurrentLanguage selectionCurrentLanguageActive"
+                  : "selectionCurrentLanguage"
+              }
+              onClick={this.toggleDropdownMenu}
+            >
+              {currentLanguage === "en" && "English"}
+              {currentLanguage === "ua" && "Ukrainian"}
+              {currentLanguage === "pl" && "Polish"}
+
+              <ArrowBackIosOutlinedIcon className="arrowDropDown" />
+            </h2>
+          </div>
+
+          <ul
+            className={isActive ? "hiddenMenu hiddenMenuOpen" : "hiddenMenu"}
+            onClick={this.handleClickSelectLanguage}
+          >
+            <li className="liItemHidden" data-source="en">
+              English
+            </li>
+            <li className="liItemHidden" data-source="ua">
+              Ukrainian
+            </li>
+            <li className="liItemHidden" data-source="pl">
+              Polish
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
+
+export default RadioToggleLanguage;
--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -2,8 +2,16 @@ import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 import "./home.scss";
 import Particles from "particlesjs";
+import RadioToggleLanguage from "./Components/RadioToggleLanguage";
+import { withTranslation } from "react-i18next";
+import i18next from "i18next";
+// import axios from "axios";
 
 class Home extends Component {
+  state = {
+    language: "en",
+  };
+
   componentDidMount() {
     Particles.init({
       selector: ".backgroundEffect",
@@ -12,39 +20,62 @@ class Home extends Component {
       sizeVariations: 5,
       speed: 0.2,
     });
+
+    let value = document.querySelector("html").getAttribute("lang");
+
+    console.log(value);
+
+    // axios("").then((data) => console.log(data));
+    // this.setState({ language: value });
   }
 
-  componentWillUnmount() {}
+  changeLanguageHandler(lang) {
+    this.setState({ language: lang });
+
+    i18next.changeLanguage(lang);
+  }
 
   render() {
+    let { t } = this.props;
+    let { language } = this.state;
     return (
       <>
+        <RadioToggleLanguage
+          onChangeLanguageHandler={this.changeLanguageHandler.bind(this)}
+          currentLanguage={language}
+        />
         <canvas className="backgroundEffect"></canvas>
-        <nav>
+        <main>
           <ul className="menuHome">
             <NavLink to="/About">
-              <li data-hover="About" className="title AboutPortfolio button">
-                <span className="hiddenText">Hello.</span>
+              <li
+                data-hover={t("Presentation.10")}
+                className="title AboutPortfolio button"
+              >
+                <span className="hiddenText">{t("Presentation.1")}</span>
               </li>
             </NavLink>
             <NavLink to="/work">
-              <li data-hover="Work" className="title worksPortfolio button">
-                <span className="hiddenText">I am</span>
+              <li
+                data-hover={t("Presentation.21")}
+                className="title worksPortfolio button"
+              >
+                <span className="hiddenText">{t("Presentation.2")}</span>
               </li>
             </NavLink>
             <NavLink to="/contact">
               <li
-                data-hover="Contact"
+                data-hover={t("Presentation.32")}
                 className="title contactPortfolio button"
               >
-                <span className="hiddenText">Vladyslav</span>
+                <span className="hiddenText">{t("Presentation.3")}</span>
               </li>
             </NavLink>
           </ul>
-        </nav>
+        </main>
       </>
     );
   }
 }
 
-export default Home;
+export default withTranslation()(Home);
